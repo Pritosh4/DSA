d749413ea5ca50cbafa5570e03e1da673ba4c6d8
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2022-08-14 22:14:19 +0530

    Update Binary Search Tree.py

--- Binary Search Tree.py
+++ Binary Search Tree.py
@@ -6,7 +6,6 @@ class Node:
 
         
 # helper function which can convert a tuple with the structure ( left_subtree, key, right_subtree) to binary tree
-
 def parse_tuple(data):
     if isinstance(data, tuple) and len(data) == 3:
         node = TreeNode(data[1])
@@ -17,6 +16,16 @@ def parse_tuple(data):
     else:
         node = TreeNode(data)
     return node
+
+
+# function tree_to_tuple that converts a binary tree into a tuple representing the same tree.
+def tree_to_tuple(node):
+  if isinstance(node, TreeNode):  
+    if node.left is None and node.right is None:
+      return node.key
+
+    return (tree_to_tuple(node.left), node.key, tree_to_tuple(node.right) )
+
 
 #helper function to display all the keys in a tree-like structure for easier visualization.
 def display_keys(node, space='\t', level=0):

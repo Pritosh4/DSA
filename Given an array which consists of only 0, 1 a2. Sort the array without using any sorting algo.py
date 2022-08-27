2d4b919f5afef96c3ce05309b9c04bc9da1599fd
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2022-08-27 18:57:33 +0530

    Create Given an array which consists of only 0, 1 a2. Sort the array without using any sorting algo.py

---
+++ Given an array which consists of only 0, 1 a2. Sort the array without using any sorting algo.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p0 = 0 #Indicates the pointer for the next place where the 0 should come
+        p2 = len(nums)-1 #Indicates the pointer for the next place where the 2 should come
+        i = 0 #Pointer used to  traverse each element of the list
+        count = 0 #Counts the number of elements that are put in the right order
+        while count < len(nums):
+            # If the number of pointer i is 0 then swap with the p0 position
+            if nums[i] == 0: 
+                nums[i], nums[p0] = nums[p0], nums[i]
+                i += 1
+                p0 += 1
+            # If the number of pointer i is 1 then we ignore as the 1s will get sorted automatically
+            elif nums[i] == 1:
+                i += 1
+            # If the number of pointer i is 2 then swap with the p2 position
+            else:
+                nums[i], nums[p2] = nums[p2], nums[i]
+                p2 -= 1 #We do not increment i as the new swapped elements needs to be checked again
+            count += 1
+        

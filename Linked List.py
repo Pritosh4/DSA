840e08dc0c0a6eb236addc5e0998bd6c37b77898
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-07-10 00:30:55 +0530

    Update Linked List.py

--- Linked List.py
+++ Linked List.py
@@ -7,13 +7,13 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def print_LL(self):
+    def print_ll(self):
         n = self.head
         if n is None:
             print('Linked List is empty')
         else:
             while n is not None:
-                print(n.data)
+                print(n.data, '-->', end=' ')
                 n = n.next
 
     def add_begin(self, data):
@@ -21,9 +21,19 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next = new_node
+
 ll = LinkedList()
 ll.add_begin(10)
 ll.add_begin(20)
-ll.add_begin(44)
-ll.add_begin(50)
+ll.add_end(40)
+ll.add_end(50)
 ll.print_ll()
